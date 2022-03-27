da243b1166dd5e1497c98ee8fae1a8616771aebd
JuvitaSaini09 <juvitasaini@gmail.com>
2022-03-28 03:37:45 +0530

    added: product page

--- ecom-react/src/App.js
+++ ecom-react/src/App.js
@@ -1,11 +1,15 @@
+// import { Route,Routes } from "react-router-dom";
 import "./App.css";
 import logo from "./logo.png";
 import { Home } from "./pages/home/home";
+import {ProductPage} from "./pages/productPage/productPage"
+
 
 function App() {
   return (
     <div className="App">
-       <Home />
+       {/* <Home /> */}
+       <ProductPage />
       </div>
   );
 }
