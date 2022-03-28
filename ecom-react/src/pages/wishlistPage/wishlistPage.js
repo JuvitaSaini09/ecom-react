25c369efbbc35d978b187dea74ba413b95664d78
JuvitaSaini09 <juvitasaini@gmail.com>
2022-03-29 02:19:50 +0530

    docs : added wishlist page

---
+++ ecom-react/src/pages/wishlistPage/wishlistPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer, Navbar } from '../../components/allComponents'
+import ".../cartPage/cartPage.css"
+import "/wishlist.css"
+function Wishlist() {
+  return (
+    <>
+    <Navbar />
+    <Wishlist />
+    <Footer />
+    </>
+  )
+}
+
+export { Wishlist }
