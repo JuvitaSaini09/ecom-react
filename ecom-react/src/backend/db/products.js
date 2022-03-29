b3238b67b386bed6c4ba3e671c9ce773e4437fe6
JuvitaSaini09 <juvitasaini@gmail.com>
2022-03-29 20:07:18 +0530

    feat : added database for prouduct page

--- ecom-react/src/backend/db/products.js
+++ ecom-react/src/backend/db/products.js
@@ -1,5 +1,12 @@
 import { v4 as uuid } from "uuid";
 
+import atomicHabits from "../../images/atomic-habits.jpg"
+import fiveAmClub from "../../images/5am-club.jpg"
+import alchemist from "../../images/alchemist.jpg"
+import doTheEpicShit from "../../images/do-the-epic-shit.jpg"
+import ikigai from "../../images/ikigai.jpg"
+import thinkAndGrowRich from "../../images/think-and-grow-rich.png"
+
 /**
  * Product Database can be added here.
  * You can add products of your wish with different attributes
@@ -8,23 +15,122 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
+    src:atomicHabits,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: "300",
+    oldPrice:"600",
     categoryName: "non-fiction",
+    rating : 4.5,
+    details :"This is details of book"
+
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    src:atomicHabits,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: "300",
+    oldPrice:"600",
+    categoryName: "non-fiction",
+    rating : 4.5,
+    details :"This is details of book"
+
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    src:atomicHabits,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: "300",
+    oldPrice:"600",
+    categoryName: "non-fiction",
+    rating : 4.5,
+    details :"This is details of book"
+
   },
+  {
+    _id: uuid(),
+    src:atomicHabits,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: "300",
+    oldPrice:"600",
+    categoryName: "non-fiction",
+    rating : 4.5,
+    details :"This is details of book"
+
+  },
+  {
+    _id: uuid(),
+    src:atomicHabits,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: "300",
+    oldPrice:"600",
+    categoryName: "non-fiction",
+    rating : 4.5,
+    details :"This is details of book"
+
+  },
+  {
+    _id: uuid(),
+    src:atomicHabits,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: "300",
+    oldPrice:"600",
+    categoryName: "non-fiction",
+    rating : 4.5,
+    details :"This is details of book"
+
+  },
+  {
+    _id: uuid(),
+    src:atomicHabits,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: "300",
+    oldPrice:"600",
+    categoryName: "non-fiction",
+    rating : 4.5,
+    details :"This is details of book"
+
+  },
+  {
+    _id: uuid(),
+    src:atomicHabits,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: "300",
+    oldPrice:"600",
+    categoryName: "non-fiction",
+    rating : 4.5,
+    details :"This is details of book"
+
+  },
+  {
+    _id: uuid(),
+    src:atomicHabits,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: "300",
+    oldPrice:"600",
+    categoryName: "non-fiction",
+    rating : 4.5,
+    details :"This is details of book"
+
+  },
+  {
+    _id: uuid(),
+    src:atomicHabits,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: "300",
+    oldPrice:"600",
+    categoryName: "non-fiction",
+    rating : 4.5,
+    details :"This is details of book"
+
+  }
 ];
--- ecom-react/src/components/productListing/productListing.js
+++ ecom-react/src/components/productListing/productListing.js
@@ -1,283 +1,45 @@
 import React from 'react'
-import atomicHabits from "../../images/atomic-habits.jpg"
-import fiveAmClub from "../../images/5am-club.jpg"
-import alchemist from "../../images/alchemist.jpg"
-import doTheEpicShit from "../../images/do-the-epic-shit.jpg"
-import ikigai from "../../images/ikigai.jpg"
-import thinkAndGrowRich from "../../images/think-and-grow-rich.png"
+import {products} from "./../../backend/db/products"
+
+
 
 function ProductListing() {
   return (
-    <>
-       <h1>Testing</h1>
-  <section className="product-page">
-    <div className="card-featured">
-      <div className="card6 card">
-        <div className="card-badge">
-          <button className="heart-badge "><span className="heart">♡</span></button>
-          <a href="#">
-            <img className="card-image" alt="card-image" src={atomicHabits} />
-            <div className="card-text card6-text">
-              <header>
-                <h3>Atomic Habits</h3><button className="rate-btn"><span>★</span>4.5</button>
-              </header>
-              <div className="card-info">
-                <p>Best Seller</p>
-                <div className="price">₹. 300
-                  <span className="price-old">₹ 600</span>
-                </div>
-              </div>
-            </div>
-          </a>
-          <footer>
-            <button className="btn-cart">Add to cart</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-    <div className="card-featured">
-      <div className="card6 card">
-        <div className="card-badge">
-          <button className="heart-badge "><span className="heart">♡</span></button>
-          <a href="#">
-            <img className="card-image" alt="card-image" src={thinkAndGrowRich} />
-            <div className="card-text card6-text">
-              <header>
-                <h3>Think and Grow Rich</h3><button className="rate-btn"><span>★</span>4.5</button>
-              </header>
-              <div className="card-info">
-                <p>Finance Book</p>
-                <div className="price">₹. 300
-                  <span className="price-old">₹ 600</span>
-                </div>
-              </div>
-            </div>
-          </a>
-          <footer>
-            <button className="btn-cart">Add to cart</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-    <div className="card-featured">
-      <div className="card6 card">
-        <div className="card-badge">
-          <button className="heart-badge "><span className="heart">♡</span></button>
-          <a href="#">
-            <img className="card-image" alt="card-image" src={ikigai} />
-            <div className="card-text card6-text">
-              <header>
-                <h3>Ikigai</h3><button className="rate-btn"><span>★</span>4.5</button>
-              </header>
-              <div className="card-info">
-                <p>Learn to be Happy</p>
-                <div className="price">₹. 300
-                  <span className="price-old">₹ 600</span>
-                </div>
-              </div>
-            </div>
-          </a>
-          <footer>
-            <button className="btn-cart">Add to cart</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-    <div className="card-featured">
-      <div className="card6 card">
-        <div className="card-badge">
-          <button className="heart-badge "><span className="heart">♡</span></button>
-          <a href="#">
-            <img className="card-image" alt="card-image" src={fiveAmClub} />
-            <div className="card-text card6-text">
-              <header>
-                <h3>5am club</h3><button className="rate-btn"><span>★</span>4.5</button>
-              </header>
-              <div className="card-info">
-                <p>Wake up early</p>
-                <div className="price">₹. 300
-                  <span className="price-old">₹ 600</span>
-                </div>
-              </div>
-            </div>
-          </a>
-          <footer>
-            <button className="btn-cart">Add to cart</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-    <div className="card-featured">
-      <div className="card6 card">
-        <div className="card-badge">
-          <button className="heart-badge "><span className="heart">♡</span></button>
-          <a href="#">
-            <img className="card-image" alt="card-image" src={doTheEpicShit} />
-            <div className="card-text card6-text">
-              <header>
-                <h3>Do The Epic Shit</h3><button className="rate-btn"><span>★</span>4.5</button>
-              </header>
-              <div className="card-info">
-                <p>Best Seller</p>
-                <div className="price">₹. 300
-                  <span className="price-old">₹ 600</span>
-                </div>
-              </div>
-            </div>
-          </a>
-          <footer>
-            <button className="btn-cart">Add to cart</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-    <div className="card-featured">
-      <div className="card6 card">
-        <div className="card-badge">
-          <button className="heart-badge "><span className="heart">♡</span></button>
-          <a href="#">
-            <img className="card-image" alt="card-image" src={atomicHabits} />
-            <div className="card-text card6-text">
-              <header>
-                <h3>Atomic Habits</h3><button className="rate-btn"><span>★</span>4.5</button>
-              </header>
-              <div className="card-info">
-                <p>Best Seller</p>
-                <div className="price">₹. 300
-                  <span className="price-old">₹ 600</span>
-                </div>
-              </div>
-            </div>
-          </a>
-          <footer>
-            <button className="btn-cart">Add to cart</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-    <div className="card-featured">
-      <div className="card6 card">
-        <div className="card-badge">
-          <button className="heart-badge "><span className="heart">♡</span></button>
-          <a href="#">
-            <img className="card-image" alt="card-image" src={thinkAndGrowRich} />
-            <div className="card-text card6-text">
-              <header>
-                <h3>Think and Grow Rich</h3><button className="rate-btn"><span>★</span>4.5</button>
-              </header>
-              <div className="card-info">
-                <p>Finance Book</p>
-                <div className="price">₹. 300
-                  <span className="price-old">₹ 600</span>
-                </div>
-              </div>
-            </div>
-          </a>
-          <footer>
-            <button className="btn-cart">Add to cart</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-    <div className="card-featured">
-      <div className="card6 card">
-        <div className="card-badge">
-          <button className="heart-badge "><span className="heart">♡</span></button>
-          <a href="#">
-            <img className="card-image" alt="card-image" src={alchemist} />
-            <div className="card-text card6-text">
-              <header>
-                <h3>Alchemist</h3><button className="rate-btn"><span>★</span>4.5</button>
-              </header>
-              <div className="card-info">
-                <p>Story of a wanderer</p>
-                <div className="price">₹. 300
-                  <span className="price-old">₹ 600</span>
-                </div>
-              </div>
-            </div>
-          </a>
-          <footer>
-            <button className="btn-cart">Add to cart</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-    <div className="card-featured">
-      <div className="card6 card">
-        <div className="card-badge">
-          <button className="heart-badge "><span className="heart">♡</span></button>
-          <a href="#">
-            <img className="card-image" alt="card-image" src={thinkAndGrowRich} />
-            <div className="card-text card6-text">
-              <header>
-                <h3>Think and Grow Rich</h3><button className="rate-btn"><span>★</span>4.5</button>
-              </header>
-              <div className="card-info">
-                <p>Finanance Book</p>
-                <div className="price">₹. 300
-                  <span className="price-old">₹ 600</span>
-                </div>
-              </div>
-            </div>
-          </a>
-          <footer>
-            <button className="btn-cart">Add to cart</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-    <div className="card-featured">
-      <div className="card6 card">
-        <div className="card-badge">
-          <button className="heart-badge "><span className="heart">♡</span></button>
-          <a href="#">
-            <img className="card-image" alt="card-image" src={ikigai} />
-            <div className="card-text card6-text">
-              <header>
-                <h3>Ikigai</h3><button className="rate-btn"><span>★</span>4.5</button>
-              </header>
-              <div className="card-info">
-                <p>Learn to be Happy</p>
-                <div className="price">₹. 300
-                  <span className="price-old">₹ 600</span>
-                </div>
-              </div>
-            </div>
-          </a>
-          <footer>
-            <button className="btn-cart">Add to cart</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-    <div className="card-featured">
-      <div className="card6 card">
-        <div className="card-badge">
-          <button className="heart-badge "><span className="heart">♡</span></button>
-          <a href="#">
-            <img className="card-image" alt="card-image" src={thinkAndGrowRich} />
-            <div className="card-text card6-text">
-              <header>
-                <h3>Think and Grow Rich</h3><button className="rate-btn"><span>★</span>4.5</button>
-              </header>
-              <div className="card-info">
-                <p>Finance Book</p>
-                <div className="price">₹. 300
-                  <span className="price-old">₹ 600</span>
-                </div>
-              </div>
-            </div>
-          </a>
-          <footer>
-            <button className="btn-cart">Add to cart</button>
-          </footer>
-        </div>
-      </div>
-    </div>
-  </section>
+    <section className="product-page">
 
-    </>
+    {
+      products.map(({src,title,author,price,oldPrice,categoryName,rating,details})=>{
+  return(
+   <div className="card-featured">
+     <div className="card6 card">
+       <div className="card-badge">
+         <button className="heart-badge "><span className="heart">♡</span></button>
+         <a href="#">
+           <img className="card-image" alt="card-image" src={src} />
+           <div className="card-text card6-text">
+             <header>
+               <h3>{title}</h3><button className="rate-btn"><span>★</span>{rating}</button>
+             </header>
+             <div className="card-info">
+               <p>{categoryName},{author}</p>
+               <div className="price">₹. {price}
+                 <span className="price-old">₹ {oldPrice}</span>
+               </div>
+             </div>
+           </div>
+         </a>
+         <footer>
+           <button className="btn-cart">Add to cart</button>
+         </footer>
+       </div>
+     </div>
+   </div>
+  )
+  }
+  )
+  }
+  </section>
+ 
   )
 }
 
