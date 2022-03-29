3db0d5a6657e0008905f341b927938437c81f6cf
JuvitaSaini09 <juvitasaini@gmail.com>
2022-03-29 22:30:12 +0530

    docs : updated database file db

--- ecom-react/src/backend/db/products.js
+++ ecom-react/src/backend/db/products.js
@@ -6,6 +6,12 @@ import alchemist from "../../images/alchemist.jpg"
 import doTheEpicShit from "../../images/do-the-epic-shit.jpg"
 import ikigai from "../../images/ikigai.jpg"
 import thinkAndGrowRich from "../../images/think-and-grow-rich.png"
+import theHouseOfStrangeStories from "../../images/TheHouseOfStrangeStories.jpg"
+import HorrorsOfTheNight from "../../images/HorrorsOfTheNight.jpg"
+import mahabharata  from "../../images/mahabharata.webp"
+import unfinished  from "../../images/Unfinished.webp"
+import agniKiUdaan from "../../images/agniKiUdaan.webp"
+import theFitnessCurrency from "../../images/theFitnessCurrency.webp"
 
 /**
  * Product Database can be added here.
@@ -19,118 +25,141 @@ export const products = [
     title: "Atomic Habits",
     author: "James Clear",
     price: "300",
-    oldPrice:"600",
-    categoryName: "non-fiction",
+    oldPrice:"600", 
+    categoryName: "Self-help",
+    rating : 4.5,
+    details :"People say when you want to change your life, you need to set big goals. But they’re wrong.World-renowned habits expert James Clear has discovered a simpler system for transforming your life. He knows that lasting change comes from the compound effect of hundreds of small decisions – doing two push-ups a day, waking up five minutes early, or holding a single short phone call. He calls them atomic habits."
+
+  },
+  {
+    _id: uuid(),
+    src:fiveAmClub,
+    title: "The 5 Am Club",
+    author: "Robin Sharma",
+    price: "179",
+    oldPrice:"228",
+    categoryName: "Self-help",
+    rating : 4.1,
+    details :"This is details of book"
+
+  },
+  {
+    _id: uuid(),
+    src:theHouseOfStrangeStories,
+    title: "The House Of Strangers Stories",
+    author: "Ruskin Bond",
+    price: "270",
+    oldPrice:"400",
+    categoryName: "Horror",
+    rating :3.8,
+    details :"This is details of book"
+  },
+  {
+    _id: uuid(),
+    src:alchemist,
+    title: "The Alchemist",
+    author: "Panlo Coelho",
+    price: "170",
+    oldPrice:"251",
+    categoryName: "Novel",
+    rating : 4.6,
+    details :"This is details of book"
+
+  },
+  {
+    _id: uuid(),
+    src:doTheEpicShit,
+    title: "DO EPIC SHIT",
+    author: "Ankur Warikoo",
+    price: "248",
+    oldPrice:"298",
+    categoryName: "Non-fiction",
+    rating : 4.3,
+    details :"This is details of book"
+
+  },
+  {
+    _id: uuid(),
+    src:ikigai,
+    title: "Ikigai",
+    author: "Francesc Miralles and Hector Garcia",
+    price: "165",
+    oldPrice:"299",
+    categoryName: "Self-help",
+    rating : 4.6,
+    details :"This is details of book"
+
+  },
+  {
+    _id: uuid(),
+    src:thinkAndGrowRich,
+    title: "Think And Grow Rich",
+    author: "Napolean Hill",
+    price: "140",
+    oldPrice:"292",
+    categoryName: "Non-fiction",
     rating : 4.5,
     details :"This is details of book"
 
   },
   {
     _id: uuid(),
-    src:atomicHabits,
-    title: "Atomic Habits",
-    author: "James Clear",
-    price: "300",
-    oldPrice:"600",
-    categoryName: "non-fiction",
-    rating : 4.5,
+    src:mahabharata,
+    title: "Mahabharata",
+    author: "Pai Anant",
+    price: "1860",
+    oldPrice:"2000",
+    categoryName: "Literature",
+    rating : 4.9,
+    details :"Mahabharata (Set Of 3 Volumes), published by Amar Chitra Katha, is a comic book version of the famous Indian epic of the Mahabharata. It consists of three volumes, spanning a total of 1200-odd pages.The Mahabharata is considered to be the longest epic poem in the world. The creation of this ageless epic is traditionally attributed to the seer Vyasa. His disciple Vaishampayana narrated it in public for the first time. King Janamejaya and numerous learned sages were present at the recital. They had gathered for a 12-year-long sacred ritual called Yajna."
+
+  },
+  {
+    _id: uuid(),
+    src:unfinished,
+    title: "Unfinished",
+    author: "Jonas Priyanka Chopra",
+    price: "334",
+    oldPrice:"450",
+    categoryName: "Literature",
+    rating : 3.5,
+    details :"The most awaited autobiography by Priyanka Chopra!'I have always felt that life is a solitary journey, that we are each on a train, riding through our hours, our days, our years. We get on alone, we leave alone, and the decisions we made as we travel on the train are our responsibility alone.'In this thoughtful and revealing memoir, readers will accompany one of the world's most recognizable women on her journey of self-discovery. A remarkable life story rooted in two different worlds, Unfinished offers insights into Priyanka Chopra Jonas's childhood in India; her formative teenage years in the United States; and her return to India, where against all odds as a newcomer to the pageant world, she won the national and international beauty competitions that launched her global acting career."
+
+  },
+  {
+    _id: uuid(),
+    src:theFitnessCurrency,
+    title: "The Fitness Currency",
+    author: "Utkarsh Rai",
+    price: "244",
+    oldPrice:"350",
+    categoryName: "Fitness",
+    rating : 3.9,
     details :"This is details of book"
 
   },
   {
     _id: uuid(),
-    src:atomicHabits,
-    title: "Atomic Habits",
-    author: "James Clear",
-    price: "300",
-    oldPrice:"600",
-    categoryName: "non-fiction",
+    src:agniKiUdaan,
+    title: "Agni Ki Udaan",
+    author: "A.P.J Kalam",
+    price: "195",
+    oldPrice:"245",
+    categoryName: "Novel",
     rating : 4.5,
     details :"This is details of book"
-
-  },
-  {
-    _id: uuid(),
-    src:atomicHabits,
-    title: "Atomic Habits",
-    author: "James Clear",
-    price: "300",
-    oldPrice:"600",
-    categoryName: "non-fiction",
-    rating : 4.5,
-    details :"This is details of book"
-
-  },
-  {
-    _id: uuid(),
-    src:atomicHabits,
-    title: "Atomic Habits",
-    author: "James Clear",
-    price: "300",
-    oldPrice:"600",
-    categoryName: "non-fiction",
-    rating : 4.5,
-    details :"This is details of book"
-
-  },
-  {
-    _id: uuid(),
-    src:atomicHabits,
-    title: "Atomic Habits",
-    author: "James Clear",
-    price: "300",
-    oldPrice:"600",
-    categoryName: "non-fiction",
-    rating : 4.5,
-    details :"This is details of book"
-
-  },
-  {
-    _id: uuid(),
-    src:atomicHabits,
-    title: "Atomic Habits",
-    author: "James Clear",
-    price: "300",
-    oldPrice:"600",
-    categoryName: "non-fiction",
-    rating : 4.5,
-    details :"This is details of book"
-
-  },
-  {
-    _id: uuid(),
-    src:atomicHabits,
-    title: "Atomic Habits",
-    author: "James Clear",
-    price: "300",
-    oldPrice:"600",
-    categoryName: "non-fiction",
-    rating : 4.5,
-    details :"This is details of book"
-
-  },
-  {
-    _id: uuid(),
-    src:atomicHabits,
-    title: "Atomic Habits",
-    author: "James Clear",
-    price: "300",
-    oldPrice:"600",
-    categoryName: "non-fiction",
-    rating : 4.5,
-    details :"This is details of book"
-
-  },
-  {
-    _id: uuid(),
-    src:atomicHabits,
-    title: "Atomic Habits",
-    author: "James Clear",
-    price: "300",
-    oldPrice:"600",
-    categoryName: "non-fiction",
-    rating : 4.5,
-    details :"This is details of book"
-
   }
+,
+{
+  _id: uuid(),
+  src:HorrorsOfTheNight,
+  title: "Horrors Of The Night",
+  author: "Tome Colemon",
+  price: "590",
+  oldPrice:"650",
+  categoryName: "Horror",
+  rating : 4.7,
+  details :"This is details of book"
+}
+
 ];
